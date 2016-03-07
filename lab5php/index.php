5383f5733e59a0366be1f820c056491a99665b61
1414397 <paril98@yahoo.com>
2016-03-07 10:10:22 +0000

    css

--- lab5php/index.php
+++ lab5php/index.php
@@ -1,9 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1414397
- * Date: 07/03/2016
- * Time: 09:35
- */
-$text="hello world"
-?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello World";
+    ?>
+</p>
+</body>
+<
+</html>

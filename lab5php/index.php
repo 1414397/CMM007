e35439100b352809c3036425aac81ac02003ef00
1414397 <paril98@yahoo.com>
2016-03-08 09:24:30 +0000

    php

--- lab5php/index.php
+++ lab5php/index.php
@@ -51,6 +51,21 @@
         echo "You can't buy anything";
     }
 
+    $numberOfHobbits = 2;
+    switch ($numberOfHobbits) {
+        case 1:
+            echo "1 sad hobbit";
+            break;
+            case2:
+            echo "2 happy hobbits";
+            break;
+        case 3:
+            echo "3 hobbits area crowd";
+            break;
+        default :
+            echo "All the hobbits have gone home";
+    }
+
 
     function squareValue($number)
     {

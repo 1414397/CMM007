0d6553d523f63c28d633a28a7b5db827858f4171
1414397 <paril98@yahoo.com>
2016-03-08 09:26:10 +0000

    php

--- lab5php/index.php
+++ lab5php/index.php
@@ -66,6 +66,23 @@
             echo "All the hobbits have gone home";
     }
 
+    $wantedgood = "mugs";
+
+    switch ($wantedgood) {
+        case "specs":
+            echo "You have to be 16 to buy specs";
+            break;
+        case "mugs":
+            echo "You have to be 18 to buy mugs";
+            break;
+        case "sausage rolls":
+            echo "You have to be 21 to buy sausage rolls";
+            break;
+        default:
+            echo "You haven't selected any thing to buy";
+    }
+
+
 
     function squareValue($number)
     {

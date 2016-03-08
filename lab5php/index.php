4da9fa90a39172a88fce72e57ee2f2d4b568b457
1414397 <paril98@yahoo.com>
2016-03-08 09:21:20 +0000

    php

--- lab5php/index.php
+++ lab5php/index.php
@@ -36,6 +36,28 @@
         print"who are you";
     }
 
+    $myage = 28;
+
+    if ($myage >= 21) {
+        echo "You can buy specs, mugs, and sausage rolls";
+    }
+    elseif ($myage >= 18) {
+        echo "You can buy specs and mugs";
+    }
+    elseif ($myage >= 16) {
+        echo "You can buy specs";
+    }
+    else {
+        echo "You can't buy anything";
+    }
+
+
+    function squareValue($number)
+    {
+        echo  $number * $number;
+    }
+    $n = 6;
+    squareValue($n);
     ?>
 </p>
 </body>

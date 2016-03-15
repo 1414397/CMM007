02e8edaac81db3cc05c8ad9a6cbab27831a55112
1414397 <paril98@yahoo.com>
2016-03-15 09:37:39 +0000

    php

---
+++ marvelmovies/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Marvel Movies</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+<h1>Marvel Movies</h1>
+        <ul>
+            <li><a href=""> Display All Movies</a></li>
+            <li><a href="">Display Movies created by Marvel Studios</a> </li>
+            <li><a href="">Display Movies created after 2010</a> </li>
+            <li><a href="">Display all X-Men Films</a> </li>
+        </ul>
+
+</body>
+</html>

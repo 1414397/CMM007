aeccb6aed11b313744e677fbb68c248260d776df
1414397 <paril98@yahoo.com>
2016-03-15 10:27:08 +0000

    php

--- marvelmovies/index.php
+++ marvelmovies/index.php
@@ -8,6 +8,7 @@
 <body>
 <h1>Marvel Movies</h1>
         <ul>
+            <li><a href="index.php"> Home</a></li>
             <li><a href="allmovies.php"> Display All Movies</a></li>
             <li><a href="createdbyms.php">Display Movies created by Marvel Studios</a> </li>
             <li><a href="createdafter2010.php">Display Movies created after 2010</a> </li>

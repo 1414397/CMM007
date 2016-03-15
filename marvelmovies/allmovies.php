dfa353b4bcf4aa37e2e77b06c1069d1b0399ca46
1414397 <paril98@yahoo.com>
2016-03-15 09:53:11 +0000

    php

--- marvelmovies/allmovies.php
+++ marvelmovies/allmovies.php
@@ -5,3 +5,15 @@
  * Date: 15/03/2016
  * Time: 09:38
  */
+include("connectionfile.php");
+$sql = "SELECT * FROM marvelmovies;
+$result = mysqli_query($db, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "marvelMovieid: " . $row["marvelMovieid"]. " - yearReleased: " . $row["yearReleased"]. " " . $row["title"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+?>

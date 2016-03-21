1c5f2c72339f1d995e460934c1c41ced7f876a59
1414397 <paril98@yahoo.com>
2016-03-21 10:19:45 +0000

    php

--- GetPost/helloSelector.php
+++ GetPost/helloSelector.php
@@ -12,8 +12,8 @@
 
 
 <form action="formResponse.php.php" method="post">
-    <label>Forename</label><input type=“text” name=“forename”>
-    <label>Surname</label><input type=“text” name=“surname”>
+    <label>Forename</label><input type="text" name="forename">
+    <label>Surname</label><input type="text" name="surname">
     <input type="submit" value="Submit">
 </form>
 

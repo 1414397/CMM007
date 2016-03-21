0a6e2c8738d0d279c744d679029879a82d9c364d
1414397 <paril98@yahoo.com>
2016-03-21 10:01:07 +0000

    php

--- GetPost/helloSelector.php
+++ GetPost/helloSelector.php
@@ -5,9 +5,9 @@
 
 <h3>Where would u like to say hello?</h3>
 
-<a href="http://http://helloPrinter.php?place=Earth">Earth</a><br><br>
-<a href="http://http://helloPrinter.php?place=Mars">Mars</a><br><br>
-<a href="http://http://helloPrinter.php?place=Uranus">Uranus</a><br><br>
+<a href="helloPrinter.php?place=Earth">Earth</a><br><br>
+<a href="helloPrinter.php?place=Mars">Mars</a><br><br>
+<a href="helloPrinter.php?place=Uranus">Uranus</a><br><br>
 
 </body>
 </html>

ee0cd5919bf36db125c73dd026967161d3d86216
1414397 <paril98@yahoo.com>
2016-03-21 09:57:21 +0000

    php

---
+++ Get&Post/helloSelector.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+
+<h3>Where would u like to say hello?</h3>
+
+<a href="http://http://helloPrinter.php?place=Earth">Earth</a><br><br>
+<a href="http://http://helloPrinter.php?place=Mars">Earth<">Mars</a><br><br>
+<a href="http://http://helloPrinter.php?place=Uranus">Earth<">Uranus</a><br><br>
+
+</body>
+</html>

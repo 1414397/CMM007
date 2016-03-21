a575c72bcfc7762e260c149bd12d5637a914f06c
1414397 <1414397@rgu.ac.uk>
2016-03-21 20:56:42 +0000

    Added files via upload

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <title>form</title>
+</head>
+<body>
+
+<form>
+    <label>Persons Name</label><input type="text" name="fullname">
+    <label>Gender</label><input type="radio" name="male">
+                         <input type="radio" name="male">
+    <label>Main Superhero Power</label><input type="text" name="power">
+
+</form>
+</body>
+</html>

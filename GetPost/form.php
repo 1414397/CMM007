31c0bbb3e1f2816cc346c38427fb1e971346dc6f
1414397 <paril98@yahoo.com>
2016-03-22 09:25:45 +0000

    php

--- GetPost/form.php
+++ GetPost/form.php
@@ -7,7 +7,9 @@
 <body>
 
 <form action="formdiplay.php" method="post">
-    <label>Persons Name</label><input type="text" name="fullname"><br><br>
+    <label> Name</label>
+    <label>Forename</label><input type="text" name="forename">
+    <label>Surname</label><input type="text" name="surname"><br><br>
     <label>Gender</label><br><input type="radio" name="gender" value="male"><label>Male</label>
                          <br><input type="radio" name="gender" value="female"><label>Female</label>
     <br><br><label>Main Superhero Power</label><input type="text" name="power">

ae1f80b87e690d606abc92a37e6a7eeaf72e1a48
1414397 <paril98@yahoo.com>
2016-03-22 09:25:05 +0000

    php

--- GetPost/formdisplay.php
+++ GetPost/formdisplay.php
@@ -3,6 +3,6 @@ $forename = $_POST["forename"];
 $surname = $_POST["surname"];
 $gender=$_POST["gender"];
 $power=$_POST["power"];
-echo $forename. " " . $surname" is a  ".$gender."   has  ".$power."  power";
+echo $forename. " " . $surname." is a  ".$gender."   has  ".$power."  power";
 
 ?>

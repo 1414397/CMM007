1b848f49101dcad14a9c5af52ebd00bb5f27249e
1414397 <paril98@yahoo.com>
2016-03-22 10:37:32 +0000

    php

--- GetPost/insertsuperhero.php
+++ GetPost/insertsuperhero.php
@@ -5,7 +5,8 @@ $forename = $_POST["forename"];
 $surname = $_POST["surname"];
 $mainpower=$_POST["power"];
 
-
+echo $forename.$surname.$mainpower;
+/*
 $sql="INSERT INTO superheros(firstname,lastname,mainSuperpower) VALUES ('$forname','$surname','$mainpower')";
 
 if(mysqli_query($dp,$sql)){
@@ -15,5 +16,5 @@ else{
     echo "Error:".$sql."<br>".mysqli_error($db);
 }
 
-header("location:index.php")
+header("location:index.php")*/
 ?>

7010b3f37a35e981fcc1818da20bee10d5ef932d
1414397 <paril98@yahoo.com>
2016-04-11 09:43:41 +0100

    php

--- CookieSessions/homepage.php
+++ CookieSessions/homepage.php
@@ -5,3 +5,16 @@
  * Date: 11/04/2016
  * Time: 09:21
  */
+
+
+function displayAccessLevelInformation($accessLevel) {
+    if ($accessLevel == "standarduser") {
+        echo "<p>You are currently logged in as a standard user</p>";
+    }
+    elseif ($accessLevel == "root") {
+        echo "<p>You are currently logged in as a root user</p>";
+        echo "<p>You now have access to additional administrative features</p>";
+    }
+}
+
+?>

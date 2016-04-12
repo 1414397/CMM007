0faf740ff71a30b4e4dcf638e9b49f0dc3c006bc
1414397 <paril98@yahoo.com>
2016-04-12 09:19:44 +0100

    php

--- CookieSessions/ckecklogin.php
+++ CookieSessions/ckecklogin.php
@@ -15,7 +15,7 @@ if($username=="ami" && $password =="jiyani")
     setcookie('access_level_cookie','standarduser');
 }
 elseif($username=="1414397" && $password =="ami"){
-
+    setcookie('access_level_cookie','root');
 }
 header('Location:homepage.php');
 ?>
